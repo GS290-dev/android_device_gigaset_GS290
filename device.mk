da69b58b562d6b4a50cd571dab8d739e9ea305b7
Robert Shih <robertshih@google.com>
2019-11-25 22:25:41 -0800

    GS290: Enable v1.3 drm plugins
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: I7453b035ca29c7ee889d03de0d55e6d947b3cb77

--- device.mk
+++ device.mk
@@ -93,7 +93,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \

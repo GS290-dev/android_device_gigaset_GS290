29cebc2619d71e96c20e388ae7b0bebb4f398eb4
Erfan Abdi <erfangplus@gmail.com>
2021-01-01 21:29:28 +0330

    GS290: Inherit base.mk instead of embedded

--- omni_GS290.mk
+++ omni_GS290.mk
@@ -1,4 +1,5 @@
-$(call inherit-product, build/target/product/embedded.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
